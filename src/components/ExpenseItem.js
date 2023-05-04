a4a7914702a537f630637f37327f30fbcf88494b
satendra-patel <patelsatendra06@gmail.com>
2023-05-04 18:22:50 +0530

    Expense item added

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ExpenseItem() {
+  return (
+    <div>
+        <div>Expense Items</div>
+        <p>Food Rs 10</p>
+        <p>Petrol Rs 100</p>
+        <p>Movies Rs 200</p>
+    </div>
+  )
+}
