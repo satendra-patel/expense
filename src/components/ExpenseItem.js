bd3ff730ced426d73d0c37415d4d6c8306bdb26a
satendra-patel <patelsatendra06@gmail.com>
2023-05-05 12:22:17 +0530

    passing date with diffent component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 
 export default function ExpenseItem(props) {
+  
     
   return (
-    <div className='expense-item'>
-        <div>{props.date.toDateString()}</div>
+    <div className='expense-item'>  
+        <ExpenseDate date={props.date} />
         <div className='expense-item__description'>
             <h2>{props.title}</h2>
             <div className='expense-item__price'>
