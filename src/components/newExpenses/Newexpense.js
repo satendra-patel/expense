81d7a35d3d7598ca3b5aee5a4d129ffacc9d4f17
satendra-patel <patelsatendra06@gmail.com>
2023-05-10 00:45:48 +0530

    DIY challenge

--- src/components/newExpenses/Newexpense.js
+++ src/components/newExpenses/Newexpense.js
@@ -3,18 +3,29 @@ import ExpenseForm from './ExpenseForm'
 import './Newexpense.css';
 
 const Newexpense=(props) =>{
+
+  const [isediting,setisediting]=useState(false);
+
+  const seteditinghandler=()=>{
+    setisediting(true);
+  }
 const saveExpenseHandler=(enteredData)=>{
   const savedData={
     ...enteredData,
     id:Math.random().toString()
   }
   props.onNewexpense(savedData);
+  setisediting(false);
   console.log(savedData);
+}
+const stopEditinghandler=()=>{
+  setisediting(false);
 }
 
   return (
     <div className='new-expense '>
-        <ExpenseForm onFormsubmission={saveExpenseHandler}/>
+        {!isediting&&<button onClick={seteditinghandler}>Add new expense</button>}
+        {isediting&&<ExpenseForm onFormsubmission={saveExpenseHandler} onCancel={stopEditinghandler}/>}
     </div>
   )
 }
